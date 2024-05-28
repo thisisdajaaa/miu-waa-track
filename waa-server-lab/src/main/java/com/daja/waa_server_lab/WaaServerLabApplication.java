5837341fbe5675f8be52c66d45da2f3946406399
DAJA <33202282+DAJAKMPM@users.noreply.github.com>
2024-05-28 13:54:05 -0500

    feat: initial boilerplate

---
+++ waa-server-lab/src/main/java/com/daja/waa_server_lab/WaaServerLabApplication.java
@@ -0,0 +1,13 @@
+package com.daja.waa_server_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaaServerLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaaServerLabApplication.class, args);
+	}
+
+}
---
+++ waa-server-lab/src/test/java/com/daja/waa_server_lab/WaaServerLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daja.waa_server_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaaServerLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
