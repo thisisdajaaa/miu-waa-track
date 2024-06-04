b921de2812479f6c81bb29f3a87319215558a04d
DAJA <33202282+DAJAKMPM@users.noreply.github.com>
2024-06-04 01:41:26 -0500

    fix: revoked token in logout

--- waa-server-lab/src/main/java/com/daja/waa_server_lab/service/impl/LogoutServiceImpl.java
+++ waa-server-lab/src/main/java/com/daja/waa_server_lab/service/impl/LogoutServiceImpl.java
@@ -1,5 +1,6 @@
 package com.daja.waa_server_lab.service.impl;
 
+import com.daja.waa_server_lab.service.spec.IJWTService;
 import com.daja.waa_server_lab.service.spec.ILogoutService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -9,6 +10,12 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class LogoutServiceImpl implements ILogoutService {
+    private final IJWTService jwtService;
+
+    public LogoutServiceImpl(IJWTService jwtService) {
+        this.jwtService = jwtService;
+    }
+
     @Override
     public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
         final String authHeader = request.getHeader("Authorization");
@@ -17,6 +24,9 @@ public class LogoutServiceImpl implements ILogoutService {
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             return;
         }
+
+        final String token = authHeader.substring(7);
+        jwtService.revokeToken(token);
 
         SecurityContextHolder.clearContext();
     }
