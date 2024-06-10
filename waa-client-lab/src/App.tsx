e728bfcaa4a54e27564b4466ce8d392cb41d4eed
DAJA <33202282+DAJAKMPM@users.noreply.github.com>
2024-06-10 10:06:09 -0500

    wip: removed initial boilerplate

--- waa-client-lab/src/App.tsx
+++ waa-client-lab/src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState<number>(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="flex min-h-screen flex-col">
+      <main className="flex-1 overflow-x-auto">
+        <div className="mx-auto my-10 max-w-4xl overflow-hidden">
+          <div className="flex flex-col items-center">
+            <p className="block">{count}</p>
+            <button onClick={() => setCount((prev) => prev + 1)}>Click</button>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
